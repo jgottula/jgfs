6547f5bfb862882f93cb6199b1ff5b21831ffa01
Justin Gottula <justin@jgottula.com>
2013-01-20 20:31:21 -0800

    add CEIL macro

--- src/common/macro.h
+++ src/common/macro.h
@@ -2,8 +2,11 @@
 #define JGFS_COMMON_MACRO_H
 
 
+#define CEIL(_x, _step) ((((_x) - 1) / (_step)) + 1)
+
 #define SWAP(_a, _b) do { \
-		typeof(_a) _temp_##_a_##_b = _a; _a = _b; _b = _temp_##_a_##_b; \
+		typeof(_a) _temp_##_a_##_b = (_a); (_a) = (_b); \
+		(_b) = (_temp_##_a_##_b); \
 	} while (0);
 
 

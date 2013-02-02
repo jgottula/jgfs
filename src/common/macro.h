bbafb9f575e8dec592076f74d72859f3f34cb2d4
Justin Gottula <justin@jgottula.com>
2013-02-01 17:54:14 -0800

    fix edge case of CEIL

--- src/common/macro.h
+++ src/common/macro.h
@@ -2,7 +2,7 @@
 #define JGFS_COMMON_MACRO_H
 
 
-#define CEIL(_x, _step) ((((_x) - 1) / (_step)) + 1)
+#define CEIL(_x, _step) ((_x) == 0 ? 0 : ((((_x) - 1) / (_step)) + 1))
 
 #define SWAP(_a, _b) do { \
 		typeof(_a) _temp_##_a_##_b = (_a); (_a) = (_b); \

5572ecabf8141f98b0f52e789344597a92822487
Justin Gottula <justin@jgottula.com>
2013-02-09 02:52:37 -0800

    add -o allow_other to fuse invocation

--- src/fuse/main.c
+++ src/fuse/main.c
@@ -17,12 +17,14 @@ int main(int argc, char **argv) {
 	dev_path = argv[1];
 	
 	/* fuse's command processing is inflexible and useless */
-	int real_argc = 4;
+	int real_argc = 6;
 	char **real_argv = malloc(real_argc * sizeof(char *));
 	real_argv[0] = argv[0];
 	real_argv[1] = strdup("-s");
 	real_argv[2] = strdup("-d");
-	real_argv[3] = argv[2];
+	real_argv[3] = strdup("-o");
+	real_argv[4] = strdup("allow_other");
+	real_argv[5] = argv[2];
 	
 	return fuse_main(real_argc, real_argv, &jg_oper, NULL);
 }
